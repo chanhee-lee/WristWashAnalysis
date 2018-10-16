107be5503da0fb52fc8b129e3e9b5ec055f7b252
Chan Lee <clee3243@gmail.com>
2018-10-15 20:28:04 -0400

    temp data

--- Program/task1.py
+++ Program/task1.py
@@ -11,33 +11,21 @@ def count_steps(data):
     plot_data(data)
     mag = vector_magnitude(data)
     plot_mag(mag)
-    average = moving_average(data, 100)
+    average = moving_average(data, 10)
     plot_mag(average)
     num_steps = 0
     '''
     ADD YOUR CODE HERE. This function counts the number of steps in data and returns the number of steps
     '''
-    i = 0
-    found = False
-    stepArray = []
-    for x in average: 
-        if (x >= 6.5 and x <= 6.55): 
-            if found == False:
-                num_steps = num_steps+1
-                stepArray.append(i)
-                found = True
-            else:  
-                found = False
-        i = i + 1
-
-    plot_steps(average, stepArray)
 
     return num_steps
 
 def run():
     # Get data
-    file_name = "task1.csv"  # Change to your file name
+    file_name = "fusrodah.csv"  # Change to your file name
+    print "YO"
     data = androidParser.get_data(file_name)
+    print "YOYO"
     number_of_steps = count_steps(data)
     print "Number of steps counted are :", number_of_steps
 
