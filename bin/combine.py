65a82e62574232729ddb621dac92fb4932cc9e98
Joshua Kim <dearjoshua98@gmail.com>
2018-10-22 23:49:41 -0400

    added other csv files

--- bin/combine.py
+++ bin/combine.py
@@ -5,7 +5,7 @@ import csv
 
 # -------------------------------------------------------------------------------
 #
-# 			Combines accelerometer and gyroscope data into another file
+# 	Combines accelerometer and gyroscope data into another file ACCEL_GYRO.csv
 #
 # -------------------------------------------------------------------------------
 
@@ -19,27 +19,35 @@ gyroX = []
 gyroY = []
 gyroZ = []
 
-
 with open("../raw/combine_accel_gyro/ACCEL_GYRO.csv", 'wb') as csvoutputfile:
 
     csvwriter = csv.writer(csvoutputfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     # loads accel data into lists
-    with open('../raw/accel/ACCEL16', 'rb') as csvinputfile:
+    with open('../raw/accel/ACCEL16.csv', 'rb') as csvinputfile:
 
         csvreader = csv.reader(csvinputfile, delimiter=',', quotechar='|')
-
         for row in csvreader:
             accelX.append(float(row[1]))
             accelY.append(float(row[2]))
             accelZ.append(float(row[3]))
 
     # loads gyro data into lists
-    with open('../raw/gyro/GYRO16', 'rb') as csvinputfile:
+    with open('../raw/gyro/GYRO16.csv', 'rb') as csvinputfile:
 
-        csvreader = csv.reader(csvinputfile, delimter=',', quotechar='|')
+        csvreader = csv.reader(csvinputfile, delimiter=',', quotechar='|')
 
         for row in csvreader:
             gyroX.append(float(row[1]))
             gyroY.append(float(row[2]))
             gyroZ.append(float(row[3]))
+
+    for i in range(0, len(accelX), 1):
+        row = []
+        row.append(accelX[i])
+        row.append(accelY[i])
+        row.append(accelZ[i])
+        row.append(gyroX[i])
+        row.append(gyroY[i])
+        row.append(gyroZ[i])
+        csvwriter.writerow(row)
