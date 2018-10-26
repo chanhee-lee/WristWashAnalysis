8b6a065de180c381635bcb7c42d316de96b1b0c1
Chan Lee <clee3243@gmail.com>
2018-10-26 18:49:57 -0400

    smooth_scale testing needed

--- bin/smooth_scale.py
+++ bin/smooth_scale.py
@@ -3,12 +3,23 @@ from scipy.signal import *
 from sklearn import preprocessing
 from numpy import * 
 import numpy as np
+
 from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("pnumber")
+args = parser.parse_args()
 
 # -----------------------------------------------------------------------------------
 #	Processes data through zero mean variance 
-#   values: n x 3 data 
+#   values: 1D array of values 
 # -----------------------------------------------------------------------------------
+def zmv(values): 
+    smooth_values = preprocessing.scale(values)
+    return smooth_values
+
+"""
+# 3 Dimensional Version 
 def zmv(values):
     x_raw = []
     y_raw = []
@@ -30,6 +41,8 @@ def zmv(values):
         all_scaled.append(z_scaled[i])
 
     return all_scaled
+"""
+
 
 # -----------------------------------------------------------------------------------
 #	Processing 
@@ -38,3 +51,39 @@ def zmv(values):
 # read from file 
 # pass into zmv 
 # write to file 
+
+x_labeled = []
+y_labeled = []
+z_labeled = []
+
+##############
+parser = ArgumentParser()
+parser.add_argument("pnumber")
+args = parser.parse_args()
+print "smooth_scale.py"
+print args.pnumber
+
+filename = "../raw/datafiles/combine/tc_accel_gyro_label_" + args.pnumber
+
+# Read data from a text file
+all_cols = genfromtxt( filename, comments='#', delimiter=",")
+
+# Process only the data from accel, ignore the timestamps
+data_cols = all_cols[:,2:]
+
+data_cols_smoothened_0 = zmv(data_cols[:,0])
+data_cols_smoothened_1 = zmv(data_cols[:,1])
+data_cols_smoothened_2 = zmv(data_cols[:,2])
+
+data_cols_smoothened = data_cols_smoothened_0
+data_cols_smoothened = column_stack((data_cols_smoothened, data_cols_smoothened_1))
+data_cols_smoothened = column_stack((data_cols_smoothened, data_cols_smoothened_2))
+"""
+# Normalize
+data_cols_smoothened_normalized = preprocessing.normalize(data_cols_smoothened, norm='l2')
+"""
+
+# Add relative timestamp
+data_cols_smoothened_final = column_stack((all_cols[:,1],data_cols_smoothened))
+
+savetxt("../raw/datafiles/combine/tc_accel_gyro_label_" + args.pnumber, data_cols_smoothened_final, delimiter=",")
