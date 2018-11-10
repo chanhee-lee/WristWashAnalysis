8e26375fd1a13df279e50e9f733d44f86b3c9c38
Chan Lee <clee3243@gmail.com>
2018-11-10 00:35:39 -0500

    Analyze Updates

--- bin/analysis/analyze_run.py
+++ bin/analysis/analyze_run.py
@@ -4,9 +4,10 @@ import evaluate
 trainfilename = "../../raw/total_gestures_combined.csv" # for all participant
 #trainfilename = "../../raw/participant3/gesture_combined3.csv"
 savename = "../../model/model_test.pkl"
-testfilename = "../../raw/participant6/gesture_combined6.csv"
+testfilename = "../../raw/participant5/gesture_combined5.csv"
 
 # Build and Evaluate for Total Gesture 
 build_model.bm(trainfilename, savename)
+print "Model Used: " + str(trainfilename)
 print "Score: " + str(evaluate.ea(savename, testfilename))
 print "For: " + str(testfilename)
